bb7cf67d32fb038c003d1a9b303de997af2107a4
Serega <sergeykyz19998@gmail.com>
2024-01-17 15:55:57 +0200

    changed custom validator

--- app/utils/validator.js
+++ app/utils/validator.js
@@ -1,7 +1,4 @@
-const { CustomError } = require("./error");
-
-const validationNameError = "Validation error";
-const validationParamsError = "Validation params error";
+// TODO - maybe need to delete this file, because unused
 
 const isEmpty = (val) => {
   if (isBoolean(val)) {
@@ -19,119 +16,40 @@ const isEmpty = (val) => {
   return !val;
 };
 
-const isUndefined = (val) => {
-  return val === undefined;
-};
+const isUndefined = (val) => val === undefined;
 
-const isNull = (val) => {
-  return val === null;
-};
+const isNull = (val) => val === null;
 
-const isBoolean = (val) => {
-  return typeof val === "boolean";
-};
+const isBoolean = (val) => typeof val === "boolean";
 
-const isString = (val) => {
-  return typeof val === "string";
-};
+const isString = (val) => typeof val === "string";
 
-const isInteger = (val) => {
-  return Number.isInteger(+val);
-};
+const isInteger = (val) => Number.isInteger(val);
 
-const isNumber = (val) => {
-  return typeof val === "number" && !Number.isNaN(val);
-};
+const isNumber = (val) =>  !isInteger(val) && !isInfinity(val) && !Number.isNaN(val) && typeof val === "number";
 
-const isBigint = (val) => {
-  return typeof val === "bigint";
-};
+const isBigint = (val) => typeof val === "bigint";
 
-const isArray = (val) => {
-  return Array.isArray(val);
-};
+const isInfinity = (val) => val === Infinity;
 
-const isObject = (val) => {
-  return !isNull(val) && !isArray(val) && typeof val === "object";
-};
+const isArray = (val) => Array.isArray(val);
 
-const isFunction = (val) => {
-  return typeof val === "function";
-};
+const isObject = (val) => !isNull(val) && !isArray(val) && typeof val === "object";
 
-const isBetween = (val, { min = 0, max }) => {
-  if (!isNumber(min) || (!isNumber(max) && !isUndefined(max)) || max < min) {
-    throw new CustomError("invalid min and/or max value specified")
-      .setName(validationParamsError)
-      .setStatus(400);
-  }
+const isFunction = (val) => typeof val === "function";
 
-  let numValue;
-  if (isString(val) || isArray(val)) {
-    numValue = val.length;
-  }
-  if (isObject(val)) {
-    numValue = Object.keys(val).length;
-  }
-  if (isNumber(val) || isBigint(val)) {
-    numValue = val;
-  }
-  if (isUndefined(val) || isNull(val)) {
-    numValue = 0;
-  }
-  if (isBoolean(val)) {
-    numValue = +val;
-  }
-
-  if (isUndefined(numValue)) {
-    throw new CustomError("cannot define numerical equivalent of the value")
-      .setName(validationNameError)
-      .setStatus(400);
-  }
-
-  const fitsMinVal = numValue >= min;
-  const fitsMaxVal = isUndefined(max) || numValue <= max;
-  return fitsMinVal && fitsMaxVal;
-};
-
-const isPossibleObjectKeys = (val, possibleKeysArr) => {
-  if (!isObject(val)) {
-    throw new CustomError("first value not object")
-      .setName(validationNameError)
-      .setStatus(400);
-  }
-
-  if (!isArray(possibleKeysArr)) {
-    throw new CustomError("second value not array")
-      .setName(validationNameError)
-      .setStatus(400);
-  }
-
-  const keys = Object.keys(val);
-
-  for (const keyItem of keys) {
-    if (!possibleKeysArr.includes(keyItem)) {
-      return false;
-    }
-  }
-
-  return true;
-};
 
 module.exports = {
-  defaults: {
-    isEmpty,
-    isUndefined,
-    isNull,
-    isBoolean,
-    isString,
-    isInteger,
-    isNumber,
-    isBigint,
-    isArray,
-    isObject,
-    isFunction,
-    isBetween,
-    isPossibleObjectKeys,
-  },
+  isEmpty,
+  isUndefined,
+  isNull,
+  isBoolean,
+  isString,
+  isInteger,
+  isNumber,
+  isBigint,
+  isInfinity,
+  isArray,
+  isObject,
+  isFunction,
 };
