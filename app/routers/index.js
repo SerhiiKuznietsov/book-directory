0e928865740a01a6e5be80afc27aa3ffd0425117
Serega <sergeykyz19998@gmail.com>
2024-02-06 17:39:24 +0200

    removed "hello world" route

--- app/routers/index.js
+++ app/routers/index.js
@@ -2,11 +2,6 @@ const rootRouter = require("express").Router();
 const { rootAuthenticate } = require("../middlewares/auth");
 const apiRouter = require('./api');
 
-rootRouter.get('/', (req, res) => {
-  res.json('hello world');
-  // TODO - remove after testing
-})
-
 // TODO - add authorization logic before authentication middleware
 rootRouter.use(rootAuthenticate);
 rootRouter.use('/api', apiRouter);
