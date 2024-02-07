e0136e4352565315ccdda8a2f66b66d0755def9f
Serega <sergeykyz19998@gmail.com>
2024-02-07 12:05:24 +0200

    added "test" mode

--- app/config/server.js
+++ app/config/server.js
@@ -1,7 +1,8 @@
-const { DEV_MODE, PROD_MODE } = require('../constants/server-mode');
+const { DEV_MODE, PROD_MODE, TEST_MODE } = require("../constants/server-mode");
 
 module.exports = {
   PORT: process.env.PORT || 80,
   IS_DEV: process.env.NODE_ENV === DEV_MODE,
   IS_PROD: process.env.NODE_ENV === PROD_MODE,
+  IS_TEST: process.env.NODE_ENV === TEST_MODE,
 };
--- app/constants/server-mode.js
+++ app/constants/server-mode.js
@@ -1,7 +1,9 @@
 const DEV_MODE = "development";
 const PROD_MODE = "production";
+const TEST_MODE = "testing";
 
 module.exports = {
   DEV_MODE,
   PROD_MODE,
+  TEST_MODE,
 };
