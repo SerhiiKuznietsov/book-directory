95a1b0cc1d47dd5f272777628867920a0e5f44e0
Serega <sergeykyz19998@gmail.com>
2024-02-15 00:50:11 +0200

    fixed "tokenMethodsFactory" code logic

--- app/services/token/jwt.js
+++ app/services/token/jwt.js
@@ -2,14 +2,10 @@ const jwt = require("jsonwebtoken");
 const algorithm = "HS512";
 const expiresIn = 60 * 60 * 24;
 
-exports.tokenMethodsFactory = (privateKey, options) => {
-  if (!options.algorithm) {
-    options.algorithm = algorithm;
-  }
+exports.tokenMethodsFactory = (privateKey, options = {}) => {
+  options.algorithm = options.algorithm || algorithm;
 
-  if (!options.expiresIn) {
-    options.expiresIn = expiresIn;
-  }
+  options.expiresIn = options.expiresIn || expiresIn;
 
   const createToken = (payload) => {
     const token = jwt.sign(payload, privateKey, options);
