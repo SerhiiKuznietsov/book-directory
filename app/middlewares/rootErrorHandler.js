a781249aab32ce29c411aecd0f52ef3b58a31ba8
Serega <sergeykyz19998@gmail.com>
2024-02-15 22:50:24 +0200

    fixed "rootErrorHandlers"

--- app/middlewares/rootErrorHandler.js
+++ app/middlewares/rootErrorHandler.js
@@ -2,21 +2,18 @@ const { CustomError } = require("../utils/error");
 
 exports.rootErrorHandlers = (app) => {
   app
-    .use('*', (req, res, next) => {
+    .use((req, res, next) => {
       const err = new CustomError(
         `url: "${req.originalUrl}" not found`
       ).setStatus(404);
 
-      // TODO - The error does not get to the next handler
       next(err);
     })
-    .use((err, req, res) => {
+    .use((err, req, res, next) => {
       console.error(err);
-
-      // TODO - fill logic
 
       const { name = "error", message = "something wrong", status = 500 } = err;
 
-      res.json({ name, message, status });
+      res.status(status).json({ name, message, status });
     });
 };
