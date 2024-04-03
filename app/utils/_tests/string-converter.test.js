472ce6f5952d5821ff70708a260abadf7d64d07f
Serega <sergeykyz19998@gmail.com>
2024-04-03 10:13:52 +0000

    added simple implementation of unit tests

---
+++ app/utils/_tests/string-converter.test.js
@@ -0,0 +1,33 @@
+const { capitalize, slugify } = require("../string-converter");
+
+describe('slugify', () => {
+  test('should return "test-word"', () => {
+    expect(slugify('test word')).toEqual('test-word');
+  });
+
+  test('should return "test-word"', () => {
+    expect(slugify(' test word ')).toEqual('test-word');
+  });
+
+  test('should return "test-word"', () => {
+    expect(slugify('test_word')).toEqual('test-word');
+  });
+
+  test('should return "test-word"', () => {
+    expect(slugify(['test', 'data_word'])).toEqual('test-data-word');
+  });
+});
+
+describe('capitalize', () => {
+  test('should return "Test word"', () => {
+    expect(capitalize('test word')).toEqual('Test word');
+  });
+
+  test('should return " test word"', () => {
+    expect(capitalize(' test word')).toEqual(' test word');
+  });
+
+  test('should return " test word "', () => {
+    expect(capitalize(' test word ')).toEqual(' test word ');
+  });
+});
