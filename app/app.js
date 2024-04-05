ee88e93203fc4a42c2cdc64bc77f3a294b6d4454
Serega <sergeykyz19998@gmail.com>
2024-04-05 17:12:52 +0000

    add logger for express middlware

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const { rootErrorHandlers } = require('./middlewares/rootErrorHandler');
+const appLogger = require('./middlewares/logger');
 const { rootRouter } = require('./routers');
+const { PORT } = require('./config/server');
 
 const app = express();
 
 // TODO - add security logic (helmet, cors)
 // TODO - add swagger
 
+app.set('port', PORT);
+app.use(appLogger)
 app.disable('x-powered-by');
 app.use(cookieParser());
 app.use(express.json());
