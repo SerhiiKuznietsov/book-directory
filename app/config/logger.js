9e5c76a65605faf4aa661cb0a507af4b2daa6c25
Serega <sergeykyz19998@gmail.com>
2024-04-05 17:26:12 +0000

    missing requirement has been added

--- app/config/logger.js
+++ app/config/logger.js
@@ -1,4 +1,5 @@
 const { LOG_LEVEL_INFO } = require('../constants/logger');
+const { validLoggerConfig } = require('../validations/config/logger');
 
 module.exports = {
   level: process.env.LOG_LEVEL || LOG_LEVEL_INFO,
