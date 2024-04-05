119d5fcc492ce673be6c1dae6ba3e7f6ce8d6f9c
Serega <sergeykyz19998@gmail.com>
2024-04-05 17:13:24 +0000

    fixed db validation

--- app/validations/config/db.js
+++ app/validations/config/db.js
@@ -1,23 +1,33 @@
 const Joi = require('joi');
 const { vld } = require('../../utils/validator-wrapper');
 
-const validDbConfig = (dbConfig) => {
-  const { error } = Joi.object({
-    username: Joi.string().required(),
-    password: Joi.string().required(),
-    database: Joi.string().required(),
-    host: Joi.string().required(),
-    port: Joi.number().required(),
-    dialect: Joi.string().required(),
-  }).validate(dbConfig);
+exports.validDbConfig = vld((config) => {
+  const schemaDbConfig = Joi.object({
+    username: Joi.string().lowercase(),
+    password: Joi.string(),
+    database: Joi.string(),
+    host: Joi.string(),
+    port: Joi.number(),
+    dialect: Joi.string(),
+    logMode: Joi.boolean(),
+    migrationStorageTableSchema: Joi.string().lowercase(),
+    migrationStorageTableName: Joi.string().lowercase(),
+    seederStorage: Joi.string().lowercase(),
+    seederStorageTableName: Joi.string().lowercase(),
+    seederStorageTableSchema: Joi.string().lowercase(),
+  }).required();
+
+  const itemSchema = schemaDbConfig.concat(
+    Joi.object({
+      development: schemaDbConfig,
+      test: schemaDbConfig,
+      production: schemaDbConfig,
+    }).required()
+  );
+
+  const { error } = itemSchema.validate(config, {
+    convert: false,
+  });
 
   if (error) throw error;
-};
-
-exports.validDbConfig = vld((config) => {
-  // TODO - fixed validator
-  // const { development, test, production } = config;
-  // validDbConfig(development);
-  // validDbConfig(test);
-  // validDbConfig(production);
 });
