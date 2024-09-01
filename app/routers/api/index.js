0b208f219c4fd88f346e7e489c087f29d18327fe
Serega <sergeykyz19998@gmail.com>
2024-09-02 01:49:21 +0300

    temp fix

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -1,15 +1,15 @@
 const bookRouter = require('./book');
-const policyRouter = require('./policy');
+// const policyRouter = require('./policy');
 const roleRouter = require('./role');
 const userRouter = require('./user');
 const authRouter = require('./auth');
-const rolePolicyRouter = require('./role-policy');
+// const rolePolicyRouter = require('./role-policy');
 
 module.exports = async (app) => {
   app.register(authRouter, { prefix: '/auth' });
   // .register(rolePolicyRouter, { prefix: '/role-policy' })
   app.register(bookRouter, { prefix: '/book' });
-  app.register(policyRouter, { prefix: '/policy' })
+  // app.register(policyRouter, { prefix: '/policy' })
   app.register(roleRouter, { prefix: '/role' });
   app.register(userRouter, { prefix: '/user' });
 };
