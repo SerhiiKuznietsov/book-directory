ebb88f1b33dc86381cb29ae282d7177ad358c9fa
Serega <sergeykyz19998@gmail.com>
2024-09-12 19:28:35 +0300

    added Swagger to project

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 const fastifyConstructor = require('fastify');
 const fastifyCookie = require('fastify-cookie');
+const fastifySwagger = require('@fastify/swagger');
+const fastifySwaggerUI = require('@fastify/swagger-ui')
 const { rootErrorHandlers } = require('./middlewares/rootErrorHandler');
 const { rootRouter } = require('./routers');
 const { logger } = require('./utils/logger');
@@ -17,6 +19,44 @@ const fastify = fastifyConstructor({
   logger,
   ajv,
 });
+
+const registerSwagger = (fastify) => {
+  fastify.register(fastifySwagger, {
+    routePrefix: '/docs',
+    swagger: {
+      info: {
+        title: 'API Documentation',
+        description: 'Testing the Fastify swagger API',
+        version: '1.0.0',
+      },
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'user', description: 'User related end-points' },
+        { name: 'role', description: 'Role related end-points' },
+        { name: 'book', description: 'Book related end-points' },
+      ],
+    },
+    exposeRoute: true,
+  });
+
+  fastify.register(fastifySwaggerUI, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false,
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
+};
+
+registerSwagger(fastify);
 
 fastify.register(fastifyCookie);
 fastify.register(rootRouter);
