507d7bbe24f4273944c498fab7b58876315a83e5
Serega <sergeykyz19998@gmail.com>
2025-02-26 23:19:09 +0200

    added default type for err object for "rootErrorHandlers"

--- src/api/rest/common/hooks/rootErrorHandler.js
+++ src/api/rest/common/hooks/rootErrorHandler.js
@@ -1,9 +1,10 @@
 const { mapErrorTypeToHttpCode } = require('../../../../utils/errorMapper/http');
+const { ERROR_TYPES } = require('../../../../constants/error');
 const { logger } = require('../../../../utils/logger');
 
 exports.rootErrorHandlers = (err, request, reply) => {
   logger.error(err);
-  const { name = 'error', message = 'something wrong', type } = err;
+  const { name = 'error', message = 'something wrong', type = ERROR_TYPES.INTERNAL_ERROR } = err;
 
   const statusCode = mapErrorTypeToHttpCode(type);
 
