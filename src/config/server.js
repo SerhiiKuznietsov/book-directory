07e60556b3a4183af6050b917304cd09c4241598
Serega <sergeykyz19998@gmail.com>
2025-03-01 02:24:38 +0200

    added spare value for “host” to server config

--- src/config/server.js
+++ src/config/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { DEV_MODE, PROD_MODE, TEST_MODE } = require('../constants/server-mode');
 
 module.exports = {
-  host: process.env.APP_HOST || '',
+  host: process.env.APP_HOST || '127.0.0.1',
   port: +process.env.APP_PORT || 80,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
