a074a6d981909d76f9ee643ba2afa97e932f68c4
Serega <sergeykyz19998@gmail.com>
2025-03-10 23:51:47 +0200

    fixed expiresIn time for jwt tokens

--- src/utils/token/access-token.js
+++ src/utils/token/access-token.js
@@ -1,18 +1,19 @@
 const { createToken, parseToken } = require('./jwt');
 const { ACCESS_TOKEN_SECRET } = require('../../config/server');
+const { ACCESS_TOKEN_AGE, REFRESH_TOKEN_AGE } = require('../../constants/auth');
 
 const algorithm = 'HS512';
-const expiresIn = 60 * 5;
 
 exports.createAccessToken = (payload) => {
   return createToken(payload, ACCESS_TOKEN_SECRET, {
     algorithm,
-    expiresIn,
+    expiresIn: ACCESS_TOKEN_AGE,
   });
 };
 
 exports.parseAccessToken = (payload) => {
   return parseToken(payload, ACCESS_TOKEN_SECRET, {
     algorithm,
+    expiresIn: REFRESH_TOKEN_AGE
   });
 };
