3e889d29efd1fa4f3080eeec6c8d96fe927653e3
Serega <sergeykyz19998@gmail.com>
2025-03-16 08:05:09 +0200

    changed name for ".log" file

--- src/constants/logger.js
+++ src/constants/logger.js
@@ -1,12 +1,13 @@
 const path = require('node:path');
 
+const date = new Date().toISOString().split('T')[0];
 const LEVEL_TRACE = 'trace';
 const LEVEL_DEBUG = 'debug';
 const LEVEL_INFO = 'info';
 const LEVEL_WARN = 'warn';
 const LEVEL_ERROR = 'error';
 const LEVEL_FATAL = 'fatal';
-const FILE_PATH = path.resolve('logs', `app.log`);
+const FILE_PATH = path.resolve('logs', `${date}.log`);
 const LEVELS_LIST = [
   LEVEL_FATAL,
   LEVEL_ERROR,
